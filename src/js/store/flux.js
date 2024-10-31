7433d1594cd7f6289761924919679321e574be35
Machado123LV <belenmachadolop@gmail.com>
2024-10-31 13:48:06 +0000

    Personajes

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,22 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			people: [] //indicamos el nombre del store y con corchetes le indicamos que es un array vacio//
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			obtenerPersonajes: async () => {
+				try {
+					const response = await fetch("https://www.swapi.tech/api/people")
+					const data = await response.json() //Transforma la info que llega del fetch en un objeto json//
+					setStore({ people: data.results }) //para que guarde en store para verlo de todos los archivos//
+				} catch (error) {
+					console.log(error)
+					return false
+				}
+			},
+
 		}
 	};
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import Card from "../component/card";
+import { Context } from "../store/appContext" //para ver las funciones de flux//
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+
+export const Home = () => {
+	const { store, actions } = useContext(Context) //cada vez que quiero usar flux traer useContext, importar, traer {store,actions}// 
+	// const {people} = store  si lo hicieramos asi le estamos diciendo "traeme solo el array de people"//
+	useEffect(() => {
+		actions.obtenerPersonajes()
+	}
+		, [])
+	return <div className="text-center mt-5">
+		<h1>Star Wars API Beli</h1>
+		<h1 className="mt-4">characters</h1>
+		<div className="d-flex flex-row overflow-scroll">
+			{
+				store.people.map((item, index) => {
+					return (
+						<Card key={index} name={item.name} uid={item.uid} />
+					)
+				})
+			}
+		</div>
 	</div>
-);
+};
